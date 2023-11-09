cbc00ad0604b99214fc75594dd5a21ed32fa41a4
henrihaapalasiili <henri.haapala@siili.com>
2023-11-09 10:49:31 +0200

    prevent _readonly attributes to save again

--- src/sagas/projectSaga.js
+++ src/sagas/projectSaga.js
@@ -500,6 +500,9 @@ const getChangedAttributeData = (values, initial, sections) => {
   let errorValues = false
   const wSpaceRegex = /^(\s+|\s+)$/g
   Object.keys(values).forEach(key => {
+    if(key.includes("_readonly")){
+      delete attribute_data[key]
+    }
     if (initial[key] !== undefined && isEqual(values[key], initial[key])) {
       return
     }
