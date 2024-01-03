86e2bd8e2906029a8f9d64878a9f9284815afb4d
henrihaapalasiili <henri.haapala@siili.com>
2024-01-03 16:03:05 +0200

    duplication fix

--- src/components/input/CustomInput.js
+++ src/components/input/CustomInput.js
@@ -165,15 +165,6 @@ const CustomInput = ({ input, meta: { error }, ...custom }) => {
       originalData = false
     }
 
-
-    let originalData
-    if (custom?.attributeData){
-      originalData = custom?.attributeData[input?.name]
-    }
-    else{
-      originalData = false
-    }
-
     if (event.target.value !== originalData) {
       //prevent saving if locked
       if (!readonly) {
