eedc1d701506983f48cd1e6451d43aba9415c61f
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-05-08 13:03:18 +0300

    Add loadingToken to state, fix links redirecting to login

--- src/selectors/apiSelector.js
+++ src/selectors/apiSelector.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect'
 const selectApi = state => state.api
 
 export const apiTokenSelector = createSelector(selectApi, ({ apiToken }) => apiToken)
-
+export const loadingTokenSelector = createSelector(selectApi, ({loadingToken}) => loadingToken )
 export const apiInitializedSelector = createSelector(
   selectApi,
   ({ apiInitialized }) => apiInitialized
