a079e934c971ef841264b757ef35a1c6f0252577
henrihaapalasiili <henri.haapala@siili.com>
2024-07-11 15:32:25 +0300

    KAAV-1473 hide esillaolo or lataukunta from add menu in certain phases and project sizes

--- src/components/ProjectTimeline/AddGroupModal.js
+++ src/components/ProjectTimeline/AddGroupModal.js
@@ -22,14 +22,22 @@ const AddGroupModal = ({toggleOpenAddDialog,addDialogStyle,addDialogData,closeAd
 
   return (
     <div className={toggleOpenAddDialog === true ? "vis-add-dialog" : "vis-hide-dialog"} style={addDialogStyle}>
-        <Button size="small" disabled={!(addDialogData.showPresence && allowedToEdit)} className={addDialogData.showPresence && allowedToEdit ? '' : 'disabled'} variant="supplementary" onClick={() => addNew(addDialogData.nextEsillaolo)} iconLeft={<IconPlus />}>
-            {t('project.add-new-presence')}
-        </Button>
-        {addDialogData.esillaoloReason && <span className='add-button-info'>{addDialogData.esillaoloReason === "noconfirmation" ? "Kaavoitussihteerin tulee vahvistaa aikaisempi esilläolo, jonka jälkeen voidaan lisätä uusi." : "Esilläolojen maksimimäärä on saavutettu."}</span>}
-        <Button size="small" disabled={!(addDialogData.showBoard && isAdmin)} className={addDialogData.showBoard && isAdmin ? '' : 'disabled'} variant="supplementary" onClick={() => addNew(addDialogData.nextLautakunta)} iconLeft={<IconPlus />}>
-            {t('project.add-new-board')}
-        </Button>
-        {addDialogData.esillaoloReason && <span className='add-button-info'>{addDialogData.esillaoloReason === "noconfirmation" ? "Kaavoitussihteerin tulee vahvistaa aikaisempi lautakunta, jonka jälkeen voidaan lisätä uusi." : "Lautakuntien maksimimäärä on saavutettu."}</span>}
+        {!addDialogData.hidePresence && 
+          <>
+            <Button size="small" disabled={!(addDialogData.showPresence && allowedToEdit)} className={addDialogData.showPresence && allowedToEdit ? '' : 'disabled'} variant="supplementary" onClick={() => addNew(addDialogData.nextEsillaolo)} iconLeft={<IconPlus />}>
+                {t('project.add-new-presence')}
+            </Button>
+            {addDialogData.esillaoloReason && <span className='add-button-info'>{addDialogData.esillaoloReason === "noconfirmation" ? "Kaavoitussihteerin tulee vahvistaa aikaisempi esilläolo, jonka jälkeen voidaan lisätä uusi." : "Esilläolojen maksimimäärä on saavutettu."}</span>}
+          </>
+        }
+        {!addDialogData.hideBoard &&
+          <>
+            <Button size="small" disabled={!(addDialogData.showBoard && isAdmin)} className={addDialogData.showBoard && isAdmin ? '' : 'disabled'} variant="supplementary" onClick={() => addNew(addDialogData.nextLautakunta)} iconLeft={<IconPlus />}>
+                {t('project.add-new-board')}
+            </Button>
+            {addDialogData.esillaoloReason && <span className='add-button-info'>{addDialogData.esillaoloReason === "noconfirmation" ? "Kaavoitussihteerin tulee vahvistaa aikaisempi lautakunta, jonka jälkeen voidaan lisätä uusi." : "Lautakuntien maksimimäärä on saavutettu."}</span>}
+          </>
+        }
     </div>
   );
 }
