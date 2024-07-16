775ebaa16e27f24609d8687a270e5b8ee2a8488a
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-07-16 15:42:45 +0300

    KAAV-116 Add day count calculation useEffect hook

--- src/components/input/DeadlineInfoText.js
+++ src/components/input/DeadlineInfoText.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { getFormValues, autofill } from 'redux-form'
 import { EDIT_PROJECT_TIMETABLE_FORM } from '../../constants'
 import { getFieldAutofillValue } from '../../utils/projectAutofillUtils'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
 import dayjs from 'dayjs'
 import { isNumber, isBoolean, isArray } from 'lodash'
 import PropTypes from 'prop-types'
 import { Notification } from 'hds-react'
+import { deadlinesSelector } from '../../selectors/projectSelector'
 
 const DeadlineInfoText = props => {
   const formValues = useSelector(getFormValues(EDIT_PROJECT_TIMETABLE_FORM))
   let inputValue = props.input && props.input.value
   let readonlyValue
+
+  const deadlines = props.deadlines
 
   const [current, setCurrent] = useState()
 
@@ -64,6 +67,25 @@ const DeadlineInfoText = props => {
 
   let value
 
+  useEffect(() => {
+    if (props.input?.name == 'nahtavillaolopaivien_lukumaara'){
+      let start_date, end_date
+      for (const index in props.deadlines){
+        let dl = props.deadlines[index]
+        if (dl.deadline.attribute == 'milloin_ehdotuksen_nahtavilla_alkaa_pieni'){
+          start_date = new Date(dl.date)
+        }
+        else if (dl.deadline.attribute == 'milloin_ehdotuksen_nahtavilla_paattyy'){
+          end_date = new Date(dl.date)
+        }
+      }
+      if (end_date && start_date){
+        const days = ((end_date - start_date) / 86400000) +1
+        setCurrent(days)
+      }
+    }
+  }, [deadlines])
+
   if (isNumber(current) || isBoolean(current)) {
     value = current
   } else {
@@ -95,4 +117,10 @@ DeadlineInfoText.propTypes = {
   ])
 }
 
-export default DeadlineInfoText
+const mapStateToProps = (state) => {
+  return {
+    deadlines: deadlinesSelector(state)
+  }
+}
+
+export default connect(mapStateToProps, null)(DeadlineInfoText)
