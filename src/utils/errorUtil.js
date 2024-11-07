c6a1a5e2d1d47954a316b76f68d2d28d908b2a12
Minna Honkanen <minna.honkanen@siili.com>
2024-11-07 13:59:12 +0200

    KAAV-1473 modify getErrorMessage to get the date from the error

--- src/utils/errorUtil.js
+++ src/utils/errorUtil.js
@@ -1,23 +1,32 @@
 // Function to handle dynamic error messages
-const getErrorMessage = (data) => {
-    let message = '';
-  
-    Object.keys(data).forEach(key => {
-      const value = data[key];
-  
-      // Check if the value is an array
-      if (Array.isArray(value)) {
-        // Convert array to a string (you can use different join separators like newline '\n')
-        message += `${key}: ${value.join(' ')}\n`;
-      } else {
-        // If not an array, just append the string or other type of value
-        message += `${key}: ${value}\n`;
-      }
-    });
-  
-    return message;
-  };
+const getErrorMessage = (data, format = 'default') => {
+  let message = '';
 
-  export default {
-    getErrorMessage
-}
+  Object.keys(data).forEach(key => {
+    const value = data[key];
+
+    // Check if the value is an array
+    if (Array.isArray(value)) {
+      // Convert array to a string based on the format
+      if (format === 'date') {
+        const dateMessage = value.find(msg => msg.includes("Ensimmäinen mahdollinen päivä on"));
+        if (dateMessage) {
+          // Extract the date
+          const date = dateMessage.split(' ').slice(-1)[0];
+          message += `${key}: ${date}\n`;
+        }
+      } else {
+        message += `${key}: ${value.join(' ')}\n`;
+      }
+    } else {
+      // If not an array, just append the string or other type of value
+      message += `${key}: ${value}\n`;
+    }
+  });
+
+  return message;
+};
+
+export default {
+  getErrorMessage
+};
