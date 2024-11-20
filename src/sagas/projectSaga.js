394ca807091d9f4abba14752d8d50d6fca0b061f
henrihaapalasiili <henri.haapala@siili.com>
2024-11-20 15:33:06 +0200

    KAAV-1473 test all save timetable

--- src/sagas/projectSaga.js
+++ src/sagas/projectSaga.js
@@ -769,11 +769,11 @@ function* saveProjectTimetable() {
     if(attribute_data.oikaisukehoituksen_alainen_readonly){
       delete attribute_data.oikaisukehoituksen_alainen_readonly
     }
-
+    console.log(values)
     try {
       const updatedProject = yield call(
         projectApi.patch,
-        { attribute_data },
+        { values },
         { path: { id: currentProjectId } },
         ':id/'
       )
