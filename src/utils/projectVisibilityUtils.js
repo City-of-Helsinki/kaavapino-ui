3945a10348f09261dae0e16ba0550b956736e0ad
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-12-12 15:24:28 +0200

    KAAV-2367 Replace getVisibilityBoolName group name parsing with a simpler map

--- src/utils/projectVisibilityUtils.js
+++ src/utils/projectVisibilityUtils.js
@@ -124,29 +124,38 @@ export const showField = (field, formValues, currentName) => {
 
 // Gets the name of the attribute used to control deadline group visiblity
 export const getVisibilityBoolName = (deadlineGroup) => {
-
-  if (['kaynnistys_1', 'hyvaksyminen_1', 'voimaantulo_1'].includes(deadlineGroup)){
-    return null;
+  const vis_bool_map = {
+    'kaynnistys_1': null,
+    'periaatteet_esillaolokerta_1': 'jarjestetaan_periaatteet_esillaolo_1',
+    'periaatteet_esillaolokerta_2': 'jarjestetaan_periaatteet_esillaolo_2',
+    'periaatteet_esillaolokerta_3': 'jarjestetaan_periaatteet_esillaolo_3',
+    'periaatteet_lautakuntakerta_1': 'periaatteet_lautakuntaan_1',
+    'periaatteet_lautakuntakerta_2': 'periaatteet_lautakuntaan_2',
+    'periaatteet_lautakuntakerta_3': 'periaatteet_lautakuntaan_3',
+    'oas_esillaolokerta_1': 'jarjestetaan_oas_esillaolo_1',
+    'oas_esillaolokerta_2': 'jarjestetaan_oas_esillaolo_2',
+    'oas_esillaolokerta_3': 'jarjestetaan_oas_esillaolo_3',
+    'luonnos_esillaolokerta_1': 'jarjestetaan_luonnos_esillaolo_1',
+    'luonnos_esillaolokerta_2': 'jarjestetaan_luonnos_esillaolo_2',
+    'luonnos_esillaolokerta_3': 'jarjestetaan_luonnos_esillaolo_3',
+    'luonnos_lautakuntakerta_1': 'kaavaluonnos_lautakuntaan_1',
+    'luonnos_lautakuntakerta_2': 'kaavaluonnos_lautakuntaan_2',
+    'luonnos_lautakuntakerta_3': 'kaavaluonnos_lautakuntaan_3',
+    'luonnos_lautakuntakerta_4': 'kaavaluonnos_lautakuntaan_4',
+    'ehdotus_nahtavillaolokerta_1': 'kaavaehdotus_nahtaville_1',
+    'ehdotus_nahtavillaolokerta_2': 'kaavaehdotus_uudelleen_nahtaville_2',
+    'ehdotus_nahtavillaolokerta_3': 'kaavaehdotus_uudelleen_nahtaville_3',
+    'ehdotus_nahtavillaolokerta_4': 'kaavaehdotus_uudelleen_nahtaville_4',
+    'ehdotus_lautakuntakerta_1': 'kaavaehdotus_lautakuntaan_1',
+    'ehdotus_lautakuntakerta_2': 'kaavaehdotus_lautakuntaan_2',
+    'ehdotus_lautakuntakerta_3': 'kaavaehdotus_lautakuntaan_3',
+    'ehdotus_lautakuntakerta_4': 'kaavaehdotus_lautakuntaan_4',
+    'tarkistettu_ehdotus_lautakuntakerta_1': 'tarkistettu_ehdotus_lautakuntaan_1',
+    'tarkistettu_ehdotus_lautakuntakerta_2': 'tarkistettu_ehdotus_lautakuntaan_2',
+    'tarkistettu_ehdotus_lautakuntakerta_3': 'tarkistettu_ehdotus_lautakuntaan_3',
+    'tarkistettu_ehdotus_lautakuntakerta_4': 'tarkistettu_ehdotus_lautakuntaan_4',
+    'hyvaksyminen_1': null,
+    'voimaantulo_1': null
   }
-
-  const splitGroup = deadlineGroup.split('_');
-  const iteration = splitGroup.pop();
-  const is_esillaolo = ['esillaolokerta', 'nahtavillaolokerta'].includes(splitGroup.pop());
-  const phaseName = splitGroup.join('_');
-  if (is_esillaolo) {
-    if (iteration === '1' && ['oas','ehdotus'].includes(phaseName)){
-      return null; // No bool exists, always true (handle separately)
-    }
-    if (["periaatteet", "oas", "luonnos"].includes(phaseName)){
-      return ['jarjestetaan', phaseName, 'esillaolo', iteration].join('_');
-    } else if (phaseName === "ehdotus"){
-      return 'kaavaehdotus_uudelleen_nahtaville_' + iteration;
-    }
-  } else {
-    // Known issue: tarkistettu_ehdotus_lautakuntaan_1 is sometimes missing in project data
-    // (Handle separately, should always be true)
-    const attributePhaseName = ['ehdotus', 'luonnos'].includes(phaseName)? 'kaava' + phaseName : phaseName;
-    return `${attributePhaseName}_lautakuntaan_` + iteration;
-  }
-  return null;
+  return vis_bool_map[deadlineGroup] || null;
 };
