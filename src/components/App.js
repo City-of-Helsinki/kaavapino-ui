ed315aae1591fb74ebd0bb676f6065f9fe1c304b
Henri Haapala <124666191+henrihaapalasiili@users.noreply.github.com>
2025-02-13 14:34:38 +0200

    KAAV-1627 Limit Matomo analytics to prod env (#507)

--- src/components/App.js
+++ src/components/App.js
@@ -34,16 +34,20 @@ class App extends Component {
 
   componentDidMount(){
     //Matomo analytic
-    let _paq = window._paq = window._paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      let u="//matomo.hel.fi/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '74']);
-      let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
+    const currentEnv = process.env.REACT_APP_ENVIRONMENT
+
+    if(currentEnv === 'production'){
+      let _paq = window._paq = window._paq || [];
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        let u="//matomo.hel.fi/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '74']);
+        let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    }
   }
 
   componentDidUpdate(prevProps) {
