668c092ae26de852702d405badbd8a55b3b138e1
henrihaapalasiili <henri.haapala@siili.com>
2025-05-26 16:00:32 +0300

    Revert "lang files to update when build is run"
    This reverts commit f847da71f941d8f688ef42cc263884decf6b8273.

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,6 @@ import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-const version = Date.now();
-
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
@@ -21,9 +19,7 @@ i18n
     fallbackLng: 'fi',
     lng: 'fi',
     debug: process.env.NODE_ENV === 'development',
-    backend: {
-      loadPath: `/locales/{{lng}}/{{ns}}.json?v=${version}`,
-    },
+
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
     }
