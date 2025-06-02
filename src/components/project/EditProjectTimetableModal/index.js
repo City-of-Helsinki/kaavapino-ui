883ed22dd7b87d03a69981c055e00d8cdcf49a8f
Henri Haapala <124666191+henrihaapalasiili@users.noreply.github.com>
2025-06-02 16:17:46 +0300

    Merge eresed lines fix to KAAV-2379,2471 and 3053 addition to be able to add ehdotus lautakunta back after deleting the last one (#563)

--- src/components/project/EditProjectTimetableModal/index.js
+++ src/components/project/EditProjectTimetableModal/index.js
@@ -778,7 +778,7 @@ class EditProjectTimeTableModal extends Component {
         || innerEnd && deadline.phase_name === "Tarkistettu ehdotus" && (deadline.deadlinegroup === "tarkistettu_ehdotus_lautakuntakerta_2" || deadline.deadlinegroup === "tarkistettu_ehdotus_lautakuntakerta_3" || deadline.deadlinegroup === "tarkistettu_ehdotus_lautakuntakerta_4") 
       ){
         if(formValues[deadline.attribute] && this.shouldAddSubgroup(deadline,formValues)){
-          let subgroup = this.addSubgroup(deadlines, i, numberOfPhases, null, innerEnd, dashedStyle, phaseData, deadLineGroups, nestedDeadlines, null, formValues);
+          let subgroup = this.addSubgroup(deadlines, i, numberOfPhases, null, innerEnd, innerStyle, phaseData, deadLineGroups, nestedDeadlines, null, formValues);
           [phaseData, deadLineGroups, nestedDeadlines] = subgroup;
         }
         innerEnd = false
