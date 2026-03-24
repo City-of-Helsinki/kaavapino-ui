a0d7f9cec43d51381d39c0b17781207447408396
Minna Honkanen <minna.honkanen@siili.com>
2026-03-24 12:01:54 +0200

    KAPI-199: Add missing PropTypes validation for QuickNav component

--- src/components/projectEdit/quickNav/QuickNav.jsx
+++ src/components/projectEdit/quickNav/QuickNav.jsx
@@ -503,8 +503,33 @@ export default function QuickNav({
 }
 
 QuickNav.propTypes = {
+  currentProject: PropTypes.object,
+  saveProjectBase: PropTypes.func,
+  handleCheck: PropTypes.func,
+  saving: PropTypes.bool,
+  isCurrentPhase: PropTypes.bool,
+  changePhase: PropTypes.func,
+  notLastPhase: PropTypes.bool,
+  phases: PropTypes.array,
+  switchDisplayedPhase: PropTypes.func,
+  saveProjectBasePayload: PropTypes.func,
+  setChecking: PropTypes.func,
+  hasMissingFields: PropTypes.bool,
+  isResponsible: PropTypes.bool,
+  isAdmin: PropTypes.bool,
   phase: PropTypes.number,
+  changeSection: PropTypes.func,
+  filterFieldsArray: PropTypes.array,
+  highlightedTag: PropTypes.string,
+  phasePrefix: PropTypes.string,
+  phaseTitle: PropTypes.string,
+  phaseStatus: PropTypes.string,
+  phaseColor: PropTypes.string,
+  showSections: PropTypes.func,
+  documents: PropTypes.array,
+  currentSchema: PropTypes.object,
   documentIndex: PropTypes.number,
   locationSearch: PropTypes.string,
-  currentSchema: PropTypes.object
+  isTheResponsiblePerson: PropTypes.bool,
+  showSection: PropTypes.bool
 }
